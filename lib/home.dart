1ff8969602011d6fa9fd98d225a223cb8c482ec9
HAMOUDA\Hamouda <mohamed.hamouda7897@gmail.com>
2024-07-06 11:04:51 +0300

    cleaning

--- lib/home.dart
+++ lib/home.dart
@@ -9,10 +9,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        sdfsdf
-          sdfd
-          sdf
-          sdf
 
         title: Text("Islami"),
       ),
